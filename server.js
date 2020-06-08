8cb3bf7d294017a1c67c7e2b689517453f2a040b
sorasak tonken <sorasak_to56@live.rmutl.ac.th>
2020-06-08 12:20:29 +0700

    to heroku

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("./config/express");
 const app = express();
-app.listen(8080);
-// app.listen(process.env.PORT);
+app.listen(process.env.PORT);
 module.exports = app;
 console.log("Server running at http://localhost:8080");
