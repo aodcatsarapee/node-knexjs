a154452708abdbd92dcf68513bb6d65255255870
sorasak tonken <sorasak_to56@live.rmutl.ac.th>
2020-06-10 09:50:47 +0700

    update..

--- config/express.js
+++ config/express.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodypaarser = require("body-parser");
+const SocketIO = require("socket.io");
+const port = 8080;
 module.exports = function () {
   const app = express();
   app.use(bodypaarser.urlencoded({ extended: false }));
@@ -23,5 +25,21 @@ module.exports = function () {
   require("../app/routes/menu.routes")(app);
   require("../app/routes/systemlog.routes")(app);
   require("../app/routes/log.routes")(app);
+
+  const server = app.listen(port, function () {
+    console.log('running in port http://localhost:' + port)
+  })
+  const io = SocketIO.listen(server)
+  io.on('connection', function (socket) {
+    // connected io success
+    console.log('connected : ID =>', socket.id)
+    // disconnect
+    socket.on('disconnect', function () {
+      console.log(' disconnect : ID => ' + socket.id);
+    })
+    socket.on('sendMessage', (data) => {
+      socket.broadcast.emit('sendMessage', data)
+    })
+  })
   return app;
 };
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
 const express = require("./config/express");
 const app = express();
-app.listen(process.env.PORT);
 module.exports = app;
-console.log("Server running at http://localhost:8080");
